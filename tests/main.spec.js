8bd9c25fc2905d44947a86d6a892ca94ae1a242d
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2018-09-01 14:44:58 -0300

    Commander install and test about version of CLI

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,12 +1,21 @@
 const expect = require('chai').expect;
 const exec = require('child_process').exec;
+const pkg = require('../package.json');
 const btcConverter = './src/main.js';
 
 describe('Main CLI', () => {
-    it('Should return Hello World', (done) => {
-        exec(btcConverter, (err, stdout, stderr) => {
+    it('Should return version of btc-converter-cli', (done) => {
+        exec(`${btcConverter} --version`, (err, stdout, stderr) => {
             if (err) throw err;
-            expect(stdout.replace('\n', '')).to.be.equal('Hello World!');
+            expect(stdout.replace('\n', '')).to.be.equal(pkg.version);
+            done();
+        });
+    });
+
+    it('Should return description btc-converter --help', (done) => {
+        exec(`${btcConverter} --help`, (err, stdout, stderr) => {
+            if (err) throw err;
+            expect(stdout.includes('Convert Bitcoin to any currency defined.')).to.be.true;
             done();
         });
     });
